8e6cf7da4729c3b95c257f0c52006119594fbddd
Dhiru <158731514+zxdhiru@users.noreply.github.com>
2024-03-21 19:30:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+const bgImg = "https://images.pexels.com/photos/2036656/pexels-photo-2036656.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+document.querySelector('body').background = bgImg;
+document.querySelector('body').innerText = "";
